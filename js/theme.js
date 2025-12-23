3e9821d8aaa601f5494be6a3460c9d59394db66b
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-23 16:14:03 +0300

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,14 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const root = document.documentElement;
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) root.dataset.theme = savedTheme;
+
+toggleBtn.addEventListener("click", () => {
+  const current = root.dataset.theme;
+  const newTheme = current === "dark" ? "light" : "dark";
+
+  root.dataset.theme = newTheme;
+  localStorage.setItem("theme", newTheme);
+});
 
