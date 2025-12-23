aa8f0fb9ed0cd43618b9d51739da920af12697a9
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-23 16:52:12 +0300

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,7 @@
+const menuBtn = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
 
