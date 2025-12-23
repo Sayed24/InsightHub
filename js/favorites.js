c200ad0410e29f1e3b0c8d1e74abe257a2060be6
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-23 16:18:47 +0300

    Update favorites.js

--- js/favorites.js
+++ js/favorites.js
@@ -1,1 +1,23 @@
+const list = document.getElementById("favorites-list");
+
+export function saveFavorite(city) {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  if (!favorites.includes(city)) {
+    favorites.push(city);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    renderFavorites();
+  }
+}
+
+export function renderFavorites() {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  list.innerHTML = "";
+
+  favorites.forEach(city => {
+    const li = document.createElement("li");
+    li.textContent = city;
+    li.tabIndex = 0;
+    list.appendChild(li);
+  });
+}
 
